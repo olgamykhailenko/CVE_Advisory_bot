00fc60294c12c13a83486d9306a8a1a20ebcbd55
olgamykhailenko <83500016+olgamykhailenko@users.noreply.github.com>
2021-06-24 18:16:57 +0200

    Search by OS
    added search function by OS and a Keyword

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import re
 from telebot import types
 from pycvesearch import CVESearch
 
+
 TOKEN = '1891713169:AAE2kaM1l6evpd09rYxLoYlFCaz8ehuIhio'
+
 
 bot = telebot.TeleBot(TOKEN)
 cve = CVESearch("https://cve.circl.lu")
@@ -11,7 +13,7 @@ MESSAGE_STATE = 0 # Global message state handler, can be used to figure out wher
 STATE_CONSTS = {
     "start": 0,
     "waiting_cve_input": 1,
-    "waiting" : 2
+    "keyword": 2
 }
 
 @bot.message_handler(commands=['start'])
@@ -20,8 +22,8 @@ def start(message):
     item1 = types.KeyboardButton('PC')
     item2 = types.KeyboardButton('Laptop')
     item3 = types.KeyboardButton('Smartphone')
-    item4 = types.KeyboardButton('Tablet')
-    item5 = types.KeyboardButton('Search By CVE Number Directly')
+    item4 = types.KeyboardButton('Search By CVE Number Directly')
+    item5 = types.KeyboardButton('Search By A Keyword')
 
     markup.add(item1, item2, item3, item4, item5)
 
@@ -51,17 +53,9 @@ def bot_message(message):
                 message_text += "\nAccess Vector: " + vuln['access']['vector']
                 message_text += "\nDescription:" + vuln['summary']
                 message_text += "\n\nYou can find more about this vulnerability on the following URL: https://nvd.nist.gov/vuln/detail/" + message.text
+
             MESSAGE_STATE = STATE_CONSTS["start"]
             bot.send_message(message.chat.id, message_text, reply_markup=markup)
-        
-        if MESSAGE_STATE == STATE_CONSTS["waiting"]:
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            vendor = message.text
-            url = "https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query?={vendor}&search_type=last3months".format(vendor=vendor)
-            bot.send_message(message.chat.id, url, reply_markup=markup)
-            MESSAGE_STATE = STATE_CONSTS["start"]
-                
-            
 
         elif (message.text in ('Search By CVE Number Directly')) and MESSAGE_STATE == STATE_CONSTS["start"]:
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -70,12 +64,24 @@ def bot_message(message):
             bot.send_message(message.chat.id, message_text, reply_markup=markup)
             MESSAGE_STATE = STATE_CONSTS["waiting_cve_input"]
 
-        elif (message.text in ('Search by Vendor Name')) and MESSAGE_STATE == STATE_CONSTS["start"]:
+        elif MESSAGE_STATE == STATE_CONSTS["keyword"]:
+            print("got here!")
+            print(message.text)
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            message_text = "Please Enter the vendor, you are searching for!"
+            message_text = ""
+            message_text += "Your request is: " + message.text
+            message_text += "\n\nFor possible vulnerabilities on "+message.text+" during the last three month please visit the next URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months" 
+
+            MESSAGE_STATE = STATE_CONSTS["start"]
             bot.send_message(message.chat.id, message_text, reply_markup=markup)
-            MESSAGE_STATE = STATE_CONSTS["waiting"]
-            
+             
+        elif (message.text in ('Search By A Keyword')) and MESSAGE_STATE == STATE_CONSTS["start"]:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            #exampleRequestData = cve.id('CVE-2010-3333')
+            message_text = "Please type any keyword:"
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+            MESSAGE_STATE = STATE_CONSTS["keyword"]
+
         elif message.text in ('PC', 'Laptop'):
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item1 = types.KeyboardButton('Windows')
@@ -100,11 +106,63 @@ def bot_message(message):
             item1 = types.KeyboardButton('PC')
             item2 = types.KeyboardButton('Laptop')
             item3 = types.KeyboardButton('Smartphone')
-            item4 = types.KeyboardButton('Tablet')
 
-            markup.add(item1, item2, item3, item4)
+            markup.add(item1, item2, item3)
 
             bot.send_message(message.chat.id, '⬅️ Back', reply_markup=markup)
         
+        elif message.text in ('Windows'):
+            print("got here!")
+            print("Windows")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            message_text = ""
+            message_text += "You chose " + message.text
+            message_text += "\n\nFor detailed list of possible vulnerabilities concerning "+message.text+" please visit the following URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&queryType=phrase&search_type=all" 
+            message_text += "\n\nPossible vulnerabilities on "+message.text+" during the last three month: https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months" 
+
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+
+        elif message.text in ('Linux'):
+            print("got here!")
+            print("Linux")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            message_text = ""
+            message_text += "You chose " + message.text
+            message_text += "\n\nFor detailed list of possible vulnerabilities concerning "+message.text+" please visit the following URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&queryType=phrase&search_type=all" 
+            message_text += "\n\nPossible vulnerabilities on "+message.text+" during the last three month: https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months" 
+
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+
+        elif message.text in ('MacOS'):
+            print("got here!")
+            print("MacOS")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            message_text = ""
+            message_text += "You chose " + message.text
+            message_text += "\n\nFor detailed list of possible vulnerabilities concerning "+message.text+" please visit the following URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&queryType=phrase&search_type=all" 
+            message_text += "\n\nPossible vulnerabilities on "+message.text+" during the last three month: https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months"            
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+
+        elif message.text in ('Android'):
+            print("got here!")
+            print("Android")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            message_text = ""
+            message_text += "You chose " + message.text
+            message_text += "\n\nFor detailed list of possible vulnerabilities concerning "+message.text+" please visit the following URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&queryType=phrase&search_type=all" 
+            message_text += "\n\nPossible vulnerabilities on "+message.text+" during the last three month: https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months" 
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+
+        elif message.text in ('iOS'):
+            print("got here!")
+            print("iOS")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            message_text = ""
+            message_text += "You chose " + message.text
+            message_text += "\n\nFor detailed list of possible vulnerabilities concerning "+message.text+" please visit the following URL: \nhttps://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&queryType=phrase&search_type=all" 
+            message_text += "\n\nPossible vulnerabilities on "+message.text+" during the last three month: https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query="+message.text+"&search_type=last3months" 
+            bot.send_message(message.chat.id, message_text, reply_markup=markup)
+
+
 
 bot.polling(none_stop=True)
